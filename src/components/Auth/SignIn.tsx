dacea644f184799e0bad558c3ac20c4e27c01cae
José <helloyinx@gmail.com>
2024-04-06 17:09:19 +0200

    Created states for inputs Log In.

--- src/components/Auth/SignIn.tsx
+++ src/components/Auth/SignIn.tsx
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
-
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import { Auth, signInWithEmailAndPassword } from "firebase/auth";
 export const SignIn = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const handleEmailInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setPassword(event.target.value);
+        signInWithEmailAndPassword(Auth, email, password)
+        .then((userCredentials) => {
+            console.log(userCredentials)
+        })
+    }
+
+    const submitSignIn = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+    }
+
     return (
         <div className="signin-container">
-            <form>
+            <form onSubmit={submitSignIn}>
                 <h1>Log In</h1>
-                <input type="email" placeholder="Email" value={email} />
-                <input type="password" placeholder="Password" value={password} />
+                <input type="email" placeholder="Email" value={email} onChange={handleEmailInputChange} />
+                <input type="password" placeholder="Password" value={password} onChange={handlePasswordInputChange} />
+                <button type="submit">Log In</button>
             </form>
         </div>
     );
