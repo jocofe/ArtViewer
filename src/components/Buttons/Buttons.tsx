ce2670de41f3a1111c994ac7765632f1b553d14e
José <helloyinx@gmail.com>
2024-04-09 00:55:04 +0200

    Trying solve path scss problems.

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../../styles/components/_buttons.scss";
+import "../../styles/index.scss";
 
 const sizes: any = {
     small: "btn--small",
--- src/components/Icons/icons.tsx
+++ src/components/Icons/icons.tsx
@@ -1,3 +1,5 @@
+import "../../styles/index.scss";
+
 // Heart Icons <3
 export function IconHeart() {
     return (
