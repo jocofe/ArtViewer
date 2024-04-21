bb2e667d147c0a420746bb58a26398d88b4acd65
José <helloyinx@gmail.com>
2024-04-21 22:34:22 +0200

    Api map feature.

--- src/AppLayout.tsx
+++ src/AppLayout.tsx
@@ -3,17 +3,37 @@ import { TopBar } from "./layouts/Topbar/TopBar";
 import { Footer } from "./layouts/Footer/Footer";
 import "./styles/index.scss";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { ArtItem, ArtListItem, ArtListItemFromApi } from "./models/art-list";
+
+export const mapArtApitoArtView = (art: ArtListItemFromApi): ArtListItem[] => {
+  return art.records.map((artItem: ArtItem, index: number) => {
+    return {
+      title: artItem._primaryTitle,
+      imageUrlThumbnail: artItem._images._primary_thumbnail,
+      imageUrlBase: artItem._images._iiif_image_base_url,
+      id: index + 1,
+      author: artItem._primaryMaker.name,
+      date: artItem._primaryDate,
+      location: artItem._primaryPlace
+    };
+  });
+};
 
 function AppLayout() {
   console.log('AppLayout renderizado');
 
+   const [artpieces, setArtPieces] = useState<[]>([]);
+
   useEffect (() => {
+
         const fetchArt = async () => {
             const apiURL = `https://api.vam.ac.uk/v2/objects/search?q_object_type=drawing&order_sort=asc&page=1&page_size=15&images_exist=true`;
             console.log ('llamada API');
             const response = await axios.get(apiURL);
-            console.log(response.data);
+            //setArtPieces(mapArtApitoArtView(response.data));
+            const mapArt = mapArtApitoArtView(response.data);
+            console.log(mapArt);
         };
 
         fetchArt();
---
+++ src/models/art-list.ts
@@ -0,0 +1,37 @@
+export interface ArtListItem {
+  title: string;
+  imageUrlThumbnail: string;
+  imageUrlBase: string;
+  id: number;
+  author: string;
+  date: string;
+  location: string;
+}
+
+export interface ArtListItemFromApi {
+    records: Array<object>;
+    info: object;
+    clusters: object;
+}
+
+export interface ArtItem {
+    _currentLocation: object;
+    _images: {
+        _iiif_image_base_url: string;
+        _iiif_presentation_url: string; 
+        _primary_thumbnail: string;
+        imageResolution: string;
+    };
+    _primaryDate: string;
+    _primaryImageId: string;
+    _primaryMaker: {
+        association: string;
+        name: string;
+    };
+    _primaryPlace: string;
+    _primaryTitle: string;
+    _warningTypes: object;
+    accessionNumber: string;
+    objectType: string;
+    systemNumber: string;
+}
