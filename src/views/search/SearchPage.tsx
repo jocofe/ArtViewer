adfb9914810f03a574d0d14138a3c2356a58b623
Miranda Muñoz <mirandamunllor@gmail.com>
2024-04-30 13:28:44 +0200

    Create and test first filters

--- src/views/search/SearchPage.tsx
+++ src/views/search/SearchPage.tsx
@@ -5,31 +5,41 @@ import { ArtCard } from '../../components/ArtCard/ArtCard';
 import axios from 'axios';
 import { ResultItem, ResultListFromApi, ResultsListItem } from '../../models/results-list';
 
-
 const mapResultsFromApi = (result: ResultListFromApi): ResultsListItem[] => {
   return result.records.map((resultItem: ResultItem) => {
       return {
       title: resultItem._primaryTitle,
       id: resultItem.systemNumber,
-      author:resultItem._primaryMaker.name,
+      author: resultItem._primaryMaker.name,
       date: resultItem._primaryDate,
       location: resultItem._primaryPlace,
       imageId: resultItem._primaryImageId,
       };
   });
-  };
+  };  
 
 export const SearchPage = () => {
 
   const [ searchParams ] = useSearchParams();
   const searchTerm = searchParams.get('search');
   const [searchResults, setSearchResults] = useState<ResultsListItem[] | null>(null);
-  
+
+  // FIlters ----> Logic to a useFilters hook?
+  const [ searchByAuthor, setSearchByAuthor ] = useState(false);
+  const [ searchByType, setSearchByType ] = useState(false);
+
 
 
   useEffect(() => {
     const getResults = async () => {
-        const apiURL = `https://api.vam.ac.uk/v2/objects/search?q=${searchTerm}&images_exist=true`;
+        let apiURL = `https://api.vam.ac.uk/v2/objects/search?q=${searchTerm}&images_exist=true`;
+        if (searchByAuthor) {
+          apiURL = `https://api.vam.ac.uk/v2/objects/search?q_actor=${searchTerm}&images_exist=true`;
+        }
+        else if (searchByType) {
+          apiURL = `https://api.vam.ac.uk/v2/objects/search?q_object_type=${searchTerm}&images_exist=true`;
+        }
+
         const response = await axios.get<ResultListFromApi>(apiURL);
         const mappedResults = mapResultsFromApi(response.data)
         setSearchResults(mappedResults);
@@ -40,13 +50,44 @@ export const SearchPage = () => {
     }; 
   
     getResults();
-  }, [searchTerm]);
+  }, [searchTerm, searchByAuthor, searchByType]);
+  console.log('Author Results', searchResults);
+
+
+  // Filters ----> Logic to another file
+  const handleArtistCheckBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchByAuthor(event.target.checked);
+  };
+
+  const handleTypeCheckBoxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchByType(event.target.checked);
+  };
 
 
   if (searchResults && searchResults.length > 0) {
     return (
       <div className='masonry-section'>
         <h2 className='masonry__title'>{searchTerm}</h2>
+        <div className='filters'>
+          <div>
+            <input 
+            type="checkbox" 
+            id="searchArtistCheckbox"
+            checked={searchByAuthor}
+            onChange={handleArtistCheckBoxChange}
+            />
+            <label htmlFor="searchArtistCheckbox">Search by artist</label>
+          </div>
+          <div>
+            <input 
+            type="checkbox" 
+            id="searchTechniqueCheckbox"
+            checked={searchByType}
+            onChange={handleTypeCheckBoxChange}
+            />
+            <label htmlFor="searchArtistCheckbox">Search by Type</label>
+          </div>
+        </div>
         <div className='masonry-wrapper'>
           <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 768: 2, 1200: 3, 1920: 4,}}>
             <Masonry className='masonry__columns' gutter='32px'>
