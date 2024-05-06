cd66845f6bce32f584176e713bca6b95c8b236c6
José <148952612+jocofe@users.noreply.github.com>
2024-05-06 18:12:24 +0200

    fixing

--- src/views/brand-guideline/BrandColor.tsx
+++ src/views/brand-guideline/BrandColor.tsx
@@ -5,6 +5,9 @@ export const BrandColor = () => {
             <div className="guide__intro">
                 <h1 className="guide__title">Color</h1>
             </div>
+            <div className="guide__brand">
+                <h2 className="guide__title">Brand</h2>
+            </div>
         </div>
     )
 }
