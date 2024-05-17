271612c222d763f965d88138df9476c96932528b
Miranda Muñoz <mirandamunllor@gmail.com>
2024-05-17 03:29:22 +0200

    Adding content, start styling, add form

--- src/routes.tsx
+++ src/routes.tsx
@@ -89,10 +89,6 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: '/landing',
-    element: <LandingPage />,
-  },
-  {
     path: '/signin',
     element: <SignInPage />,
   },
--- src/views/landing-page/LandingPage.tsx
+++ src/views/landing-page/LandingPage.tsx
@@ -1,3 +1,40 @@
+import { Button } from "../../components/Buttons/Buttons"
+import { SignUpForm } from "../../components/Form/SignUpForm"
+
 export const LandingPage = () => {
-  return <div>Landing Page</div>;
-};
+    
+    return (
+        <div>
+            <section className="hero">
+                <div className="hero__text">
+                    <h1>Find art to research</h1>
+                </div>
+                <div>
+                    images
+                </div>
+            </section>
+            <section className="section section--golden">
+                <div>images</div>
+                <div className="claim">
+                    <h1>Search for some art piece</h1>
+                    <h3>Are you studying some artist? Some movement? Just want to explore art? Try to explore now.</h3>
+                    <Button size="large">Explore</Button>
+                </div>
+            </section>
+            <section className="section section--wooden">
+                <div className="claim">
+                    <h1>Save your favorite art pieces</h1>
+                    <h3>Put them in collections and organize them as you please for watching later.</h3>
+                    <Button size="large">Explore</Button>
+                </div>
+                <div>
+                    Images
+                </div>
+            </section>
+            <div>
+                <h1>Register now to watch art</h1>
+                <SignUpForm/>
+            </div>
+        </div>
+    )
+}
