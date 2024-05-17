0b7385594914c7c6c171ac239abc09fd832d2c12
Miranda Muñoz <mirandamunllor@gmail.com>
2024-05-17 17:01:56 +0200

    Create RangemImage component for landing hero, ad variable text., start SignUpForm for landing

---
+++ src/components/Landing/SignUpFromLanding.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "../Form/SignUpForm"
+
+export const SignUpFromLanding = () => {
+    return (
+        <div className="sign-up-landing">
+            <SignUpForm/>
+        </div>
+    )
+}
--- src/views/landing-page/LandingPage.tsx
+++ src/views/landing-page/LandingPage.tsx
@@ -1,39 +1,53 @@
+import { useEffect, useState } from "react"
 import { Button } from "../../components/Buttons/Buttons"
-import { SignUpForm } from "../../components/Form/SignUpForm"
+import { RandomMosaicImages } from "../../components/Landing/RandomMosaicImages"
+import { SignUpFromLanding } from "../../components/Landing/SignUpFromLanding"
 
 export const LandingPage = () => {
+    const [currentTextIndex, setCurrentTextIndex] = useState(0)
+    const texts = ['research', 'explore', 'inspire', 'collect', 'discover']
+    
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentTextIndex((currentTextIndex + 1) % texts.length);
+        }, 2000);
+        
+        return () => clearInterval(interval);
+    }, [currentTextIndex, texts.length]);
     
     return (
         <div>
             <section className="hero">
                 <div className="hero__text">
-                    <h1>Find art to research</h1>
+                    <h1 id="changing-text">Find art to <span className="changing-text">{texts[currentTextIndex]}</span></h1>
                 </div>
-                <div>
-                    images
+                <div className="mosaic-images">
+                    <div className="mosaic-column">
+                        <RandomMosaicImages/>
+                    </div>
                 </div>
             </section>
             <section className="section section--golden">
                 <div>images</div>
-                <div className="claim">
+                <div className="section__claim">
                     <h1>Search for some art piece</h1>
                     <h3>Are you studying some artist? Some movement? Just want to explore art? Try to explore now.</h3>
-                    <Button size="large">Explore</Button>
+                    <Button size="large" className="landing-btn">Explore</Button>
                 </div>
             </section>
             <section className="section section--wooden">
-                <div className="claim">
+                <div className="section__claim">
                     <h1>Save your favorite art pieces</h1>
                     <h3>Put them in collections and organize them as you please for watching later.</h3>
-                    <Button size="large">Explore</Button>
+                    <Button size="large" className="landing-btn">Explore</Button>
                 </div>
                 <div>
                     Images
                 </div>
             </section>
-            <div>
-                <h1>Register now to watch art</h1>
-                <SignUpForm/>
+            <div className="register-claim section">
+                <h1 className="section__claim">Register now to watch art</h1>
+                <SignUpFromLanding/>
             </div>
         </div>
     )
