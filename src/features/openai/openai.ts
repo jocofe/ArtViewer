b130ac74ea5f96ece67c6df16fda24cb285674da
José <helloyinx@gmail.com>
2024-05-28 17:03:35 +0200

    Trying Vercel API.

--- src/features/openai/openai.ts
+++ src/features/openai/openai.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API;
+const OPENAI_API_KEY = import.process.env.VITE_OPENAI_API;
 
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
