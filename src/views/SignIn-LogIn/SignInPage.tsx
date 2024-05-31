6775ae249567dc73de0c08f71562e5d029b81135
José <helloyinx@gmail.com>
2024-05-31 17:45:53 +0200

    Sign-New-User fix random art image from Api and Url to our ArtPage.

--- src/views/SignIn-LogIn/SignInPage.tsx
+++ src/views/SignIn-LogIn/SignInPage.tsx
@@ -1,10 +1,29 @@
+import { Link } from 'react-router-dom';
 import { SignGoogle } from '../../features/authentication/SignGoogle';
 import { SignIn } from '../../features/authentication/SignIn';
+import { useGetRandomImgApi } from '../../hooks/useGetRandomImgApi';
 
 export const SignInPage = () => {
+  const apiUrl =
+    'https://api.vam.ac.uk/v2/objects/search?q=oil%20canvas&min_length=2&max_length=16&images_exist=true&order_sort=asc&page=1&page_size=15&cluster_size=20&images=false&random=false';
+  const { imageId, loading, systemNumber } = useGetRandomImgApi(apiUrl);
+
   return (
     <div className="signinpage-wrapper">
-      <div className="imgsign-wrapper"></div>
+      {!loading && imageId && (
+        <div
+          className={`imgsign-wrapper`}
+          key={imageId}
+          style={{
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'auto',
+            backgroundImage: `url(https://framemark.vam.ac.uk/collections/${imageId}/full/full/0/default.jpg)`,
+          }}
+        >
+          {loading ? <div>Loading...</div> : null}
+          <Link className="expanded-anchor" to={`/art-piece/${systemNumber}`}></Link>
+        </div>
+      )}
       <div className="sign-wrapper">
         <div className="sign__content">
           <h4 className="h4--bold">Sign In to ArtViewer</h4>
