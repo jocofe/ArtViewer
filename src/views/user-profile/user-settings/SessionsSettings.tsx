3387e55027d8d8a97b80e6e85e7d2b594cf1515f
José <148952612+jocofe@users.noreply.github.com>
2024-06-03 18:27:07 +0200

    fix css on sessions settings devices

--- src/views/user-profile/user-settings/SessionsSettings.tsx
+++ src/views/user-profile/user-settings/SessionsSettings.tsx
@@ -1,11 +1,11 @@
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../../../context/UserContextProvider";
-import { UserSessionsProps } from "../../../models/userSessions";
-import { closeSession, getUserLoginSessions } from "../../../components/Services/sessions";
-import { parseUserAgent } from "../../../components/Services/userAgentParser";
-import { useClearsMessage } from "../../../hooks/useClearMessage";
-import { auth } from "../../../config/config";
-import { useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../../context/UserContextProvider';
+import { UserSessionsProps } from '../../../models/userSessions';
+import { closeSession, getUserLoginSessions } from '../../../components/Services/sessions';
+import { parseUserAgent } from '../../../components/Services/userAgentParser';
+import { useClearsMessage } from '../../../hooks/useClearMessage';
+import { auth } from '../../../config/config';
+import { useNavigate } from 'react-router-dom';
 
 export const SessionsSettings = () => {
   const { userData } = useContext(UserContext);
@@ -13,19 +13,20 @@ export const SessionsSettings = () => {
   const navigate = useNavigate();
   const [sessions, setSessions] = useState<UserSessionsProps[]>([]);
 
-  
-   // Filter sessions by IP
-   const filterLatestSessionsByIP = (sessions: UserSessionsProps[]): UserSessionsProps[] => {
-    const sessionsMap: Record<string, UserSessionsProps> = sessions.reduce((acc, session) => {
-      const existingSession = acc[session.ipAddress];
-      if (!existingSession || session.loginTime > existingSession.loginTime) {
-        acc[session.ipAddress] = session;
-      }
-      return acc;
-    }, {} as Record<string, UserSessionsProps>);
+  // Filter sessions by IP
+  const filterLatestSessionsByIP = (sessions: UserSessionsProps[]): UserSessionsProps[] => {
+    const sessionsMap: Record<string, UserSessionsProps> = sessions.reduce(
+      (acc, session) => {
+        const existingSession = acc[session.ipAddress];
+        if (!existingSession || session.loginTime > existingSession.loginTime) {
+          acc[session.ipAddress] = session;
+        }
+        return acc;
+      },
+      {} as Record<string, UserSessionsProps>,
+    );
     return Object.values(sessionsMap);
   };
-
 
   useEffect(() => {
     const fetchSessions = async () => {
@@ -43,8 +44,6 @@ export const SessionsSettings = () => {
     fetchSessions();
   }, [userData]);
 
-
- 
   const handleCloseSession = async (sessionId: string, sessionIpAddress: string) => {
     setMessage('');
     if (userData?.email) {
@@ -55,9 +54,10 @@ export const SessionsSettings = () => {
         console.log(sessionIpAddress);
         console.log('hola', activeSessionIpAddress);
 
-        if (isCurrentSession && auth) { // Verifica si 'auth' existe
+        if (isCurrentSession && auth) {
+          // Verifica si 'auth' existe
           await auth.signOut(); // Utiliza 'auth.signOut()' para cerrar la sesión
-          navigate('/')
+          navigate('/');
           return;
         }
 
@@ -73,37 +73,52 @@ export const SessionsSettings = () => {
   };
 
   return (
-  <div className="settings">
-    <p>Your sessions</p>
-    <hr className="sessions-hr"/>
-    <p className="sessions-text">This is a list of devices that have logged into your account. Revoke any sessions that you do not recognize.</p>
-    
-    <p>Devices</p>
-    <hr className="sessions-hr"/>
-    <div>
-      {sessions.length > 0 ? (
-        sessions.map((session, index) => (
-          <div key={index}>
-            <div className="sessions-devices">
-            <div className="sessions-devices__list">
-              <p><strong>IP Address:</strong> {session.ipAddress}</p>
-              <p><strong>{parseUserAgent(session.userAgent).browser} on {parseUserAgent(session.userAgent).operatingSystem}</strong></p>
-              <p><strong>Last Access:</strong>{session.lastAccess.toLocaleString()}</p>
-              <p><strong>Login Date:</strong> {session.loginTime.toLocaleString()}</p>
+    <div className="settings">
+      <p>Your sessions</p>
+      <hr className="sessions-hr" />
+      <p className="sessions-text">
+        This is a list of devices that have logged into your account. Revoke any sessions that you do not recognize.
+      </p>
+
+      <p>Devices</p>
+      <hr className="sessions-hr" />
+      <div className="sessions-list">
+        {sessions.length > 0 ? (
+          sessions.map((session, index) => (
+            <div key={index}>
+              <div className="sessions-devices">
+                <div className="sessions-devices__list">
+                  <p>
+                    <strong>IP Address:</strong> {session.ipAddress}
+                  </p>
+                  <p>
+                    <strong>
+                      {parseUserAgent(session.userAgent).browser} on {parseUserAgent(session.userAgent).operatingSystem}
+                    </strong>
+                  </p>
+                  <p>
+                    <strong>Last Access:</strong>
+                    {session.lastAccess.toLocaleString()}
+                  </p>
+                  <p>
+                    <strong>Login Date:</strong> {session.loginTime.toLocaleString()}
+                  </p>
+                </div>
+                <div className="sessions-devices__close">
+                  {/* Agrega un botón o enlace para revocar la sesión */}
+                  <button onClick={() => handleCloseSession(session.id, session.ipAddress)} className="sessions-btn">
+                    Close Session
+                  </button>
+                </div>
+              </div>
+              <hr className="sessions-hr" />
             </div>
-            <div className="sessions-devices__close">
-            {/* Agrega un botón o enlace para revocar la sesión */}
-              <button onClick={() => handleCloseSession(session.id, session.ipAddress)} className="sessions-btn">Close Session</button>
-            </div>
-            </div>
-            <hr className="sessions-hr" />
-          </div>
-        ))
-      ) : (
-      <p>No sessions founded</p>
-    )}
-    {message && <p>{message}</p>}
+          ))
+        ) : (
+          <p>No sessions founded</p>
+        )}
+        {message && <p>{message}</p>}
+      </div>
     </div>
-  </div>
   );
 };
